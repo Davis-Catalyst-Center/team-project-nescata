799546a2abe7368ee3e0eeee9ed387df10750ee8
27dmcfadden-bit <27dmcfadden@go.dsdmail.net>
2025-09-22 15:20:23 +0000

    only accepts valid files

--- src/cart.cpp
+++ src/cart.cpp
@@ -1,6 +1,33 @@
+#include <fstream>
+#include <iostream>
 #include "cart.h"
 
 Cart::Cart(const char* filename) {
+
+    int safe = 1;
+    std::ifstream gameFile(filename, std::ios::binary);
+    while (safe == 1){
+        if (gameFile.is_open()){
+            std::cout<<"Yes"<<std::endl;
+            break;
+        }
+    }
+
+    char ch;
+
+    for (int i = 0; i < 16; i++) {
+        gameFile.get(ch);
+        header[i] = ch;
+    }
+
+    if (header[0] != 'N' || header[1] != 'E' || header[2] != 'S' || header[3] != 0x1A) {
+        std::cerr << "Not a valid iNES file!" << std::endl;
+        return;
+    }
+    for (int i = 0; i < 16; i++) {
+        gameFile.get(ch);
+        header[i] = ch;
+    }
     // Constructor implementation (e.g., load ROM file)
 }
 
--- src/cart.h
+++ src/cart.h
@@ -9,6 +9,7 @@ public:
     // vector of 0x4000 byte rom banks
     std::vector<std::array<uint8, 0x4000>> romBanks;
 
+    uint8 header[16];
     Cart(const char* filename);
     ~Cart();
 };
