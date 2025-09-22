865e65fa47dd5b02d257cd51bae85418385bd3db
27dmcfadden-bit <27dmcfadden@go.dsdmail.net>
2025-09-22 14:12:50 +0000

    Running and trying to make it read nestest.nes.

--- read.cpp
+++ read.cpp
@@ -3,12 +3,19 @@
 #include <iostream>
 int main(){
     int safe = 1;
+    std::ifstream gameFile("nestest.nes");
     while (safe == 1){
-        std::ifstream gameFile("nestest.nes");
-        if (!gameFile.is_open()){
+        if (gameFile.is_open()){
             std::cout<<"No"<<std::endl;
+            break;
         }    
+        break;
     }
+    char ch;
+    while (gameFile.get(ch)) {
+        // Process each 'ch' here
+        std::cout << ch;
+        }
     std::cout<<"Over"<<std::endl;
     return 0;
 }
