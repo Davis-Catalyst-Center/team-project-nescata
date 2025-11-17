f406e6cee2896056315e89e318616aa9f6e5c8a7
jack kagie <zillion895@gmail.com>
2025-11-17 09:12:23 -0700

    .

--- src/core/composite.cpp
+++ src/core/composite.cpp
@@ -6,7 +6,7 @@ Composite::Composite() {
 
 void Composite::renderScanline(int scanline) {
 	// fill a line of the frame buffer with a random solid color for testing
-	uint32 color = 0xFF000000 | (rand() % 0x00FFFFFF);
+	uint32 color = 0x00FFFFFF;// | (rand() % 0x00FFFFFF);
 	for (int x = 0; x < 256; x++) {
 		frame_buffer[scanline * 256 + x] = color;
 	}
