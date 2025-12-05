ab4097aa150f8d2483eddd39c1c1a4e35c7c7dba
Trevor Abercrombie <trevor.abercrombie@gmail.com>
2025-12-04 22:27:51 -0700

    fix buid scripts (windows is broken though (as usual))

--- build.sh
+++ build.sh
@@ -2,8 +2,11 @@ mkdir -p build
 
 cp /usr/x86_64-w64-mingw32/bin/SDL2.dll build
 
-x86_64-w64-mingw32-g++ -std=c++17 -Iinclude -o build/main.exe src/*.cpp src/*/*.cpp -lmingw32 -lSDL2main -lSDL2 -Wl,-Bstatic -lstdc++ -Wl,-Bdynamic -lSDL2 -static-libgcc -static-libstdc++ -mwindows && \
+x86_64-w64-mingw32-g++ -g -std=c++17 -Iinclude -o build/main.exe src/*.cpp \
+	-lmingw32 -lSDL2main -lSDL2 -Wl,-Bstatic -lstdc++ -Wl,-Bdynamic \
+	-lSDL2 -static -static-libgcc -static-libstdc++ -mwindows -lwinmm \
+	-lole32 -loleaut32 -limm32 -lsetupapi -lversion -luuid -lgdi32 -luser32 && \
 echo "compiled windows executable"
 
-g++ -std=c++17 -Iinclude -o build/main.out src/*.cpp src/*/*.cpp -lSDL2 && \
+g++ -std=c++17 -Iinclude -o build/main.out src/*.cpp -lSDL2 && \
 echo "compiled unix executable"
